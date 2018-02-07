dd9609d0a38004efafd5275fa946fd876d120a23
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-02-07 22:49:21 +0530

    Updated Post class

--- app/src/main/java/com/example/dipansh/the_boring_blog_android/Post.java
+++ app/src/main/java/com/example/dipansh/the_boring_blog_android/Post.java
@@ -6,58 +6,51 @@ package com.example.dipansh.the_boring_blog_android;
 
 public class Post {
 
-    private String status;
+    private int id;
     private String title;
-    private String content;
     private String slug;
-    private String category;
+    private String content;
     private String seoTitle;
     private String seoDescription;
-    private String author;
     private String publishedDateTime;
     private String createdDateTime;
+    private String status;
     private String musicLink;
+    private int category;
+    private int author;
 
     public Post() {
     }
 
-    public String getStatus() {
-        return status;
+    public Post(int id, String title, String slug, String content, String seoTitle, String seoDescription, String publishedDateTime, String createdDateTime, String status, String musicLink, int category, int author) {
+        this.id = id;
+        this.title = title;
+        this.slug = slug;
+        this.content = content;
+        this.seoTitle = seoTitle;
+        this.seoDescription = seoDescription;
+        this.publishedDateTime = publishedDateTime;
+        this.createdDateTime = createdDateTime;
+        this.status = status;
+        this.musicLink = musicLink;
+        this.category = category;
+        this.author = author;
     }
 
-    public void setStatus(String status) {
-        this.status = status;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public Post(String status, String title, String content, String slug, String category, String seoTitle, String seoDescription, String author, String publishedDateTime, String createdDateTime, String musicLink) {
-        this.status = status;
-        this.title = title;
-        this.content = content;
-        this.slug = slug;
-        this.category = category;
-        this.seoTitle = seoTitle;
-        this.seoDescription = seoDescription;
-        this.author = author;
-        this.publishedDateTime = publishedDateTime;
-        this.createdDateTime = createdDateTime;
-        this.musicLink = musicLink;
-    }
-
     public void setTitle(String title) {
         this.title = title;
-
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
     }
 
     public String getSlug() {
@@ -68,12 +61,12 @@ public class Post {
         this.slug = slug;
     }
 
-    public String getCategory() {
-        return category;
+    public String getContent() {
+        return content;
     }
 
-    public void setCategory(String category) {
-        this.category = category;
+    public void setContent(String content) {
+        this.content = content;
     }
 
     public String getSeoTitle() {
@@ -92,14 +85,6 @@ public class Post {
         this.seoDescription = seoDescription;
     }
 
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
     public String getPublishedDateTime() {
         return publishedDateTime;
     }
@@ -116,11 +101,35 @@ public class Post {
         this.createdDateTime = createdDateTime;
     }
 
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
     public String getMusicLink() {
         return musicLink;
     }
 
     public void setMusicLink(String musicLink) {
         this.musicLink = musicLink;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public int getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(int author) {
+        this.author = author;
     }
 }
