d23f05b1576cd304b178263283b1b2f6ae2e6b95
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-02-10 21:15:26 +0530

    Added media compatibility to post activity

--- app/src/main/java/com/example/dipansh/the_boring_blog_android/ScrollingActivity.java
+++ app/src/main/java/com/example/dipansh/the_boring_blog_android/ScrollingActivity.java
@@ -1,6 +1,8 @@
 package com.example.dipansh.the_boring_blog_android;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -9,6 +11,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +20,9 @@ import org.w3c.dom.Text;
 import java.io.IOException;
 
 public class ScrollingActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private MediaPlayer media;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,5 +53,47 @@ public class ScrollingActivity extends AppCompatActivity {
         TextView postContent = findViewById(R.id.postContent);
         postContent.setText(post.getContent());
 
+        media = new MediaPlayer();
+        media.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        try {
+            String urlText = post.getMusicLink();
+            media.reset();
+            media.setDataSource(urlText);
+            new PrepareMusicPlayer().execute(urlText);
+            progressBar.setVisibility(View.VISIBLE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private class PrepareMusicPlayer extends AsyncTask<String, Integer, String> {
+        protected String doInBackground(String... strings) {
+            try {
+                media.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onProgressUpdate(Integer... progress) {
+            //setProgressPercent(progress[0]);
+        }
+
+        protected void onPostExecute(String string) {
+            media.start();
+            progressBar.setVisibility(View.INVISIBLE);
+            Toast.makeText(ScrollingActivity.this, "Playing", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(media!=null){
+            media.release();
+        }
+        super.onBackPressed();
     }
 }
