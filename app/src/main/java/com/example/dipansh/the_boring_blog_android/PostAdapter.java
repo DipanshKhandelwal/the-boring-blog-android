a03fcf90bf958206be55a4cc369b189b70253e69
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-02-10 19:23:40 +0530

    Added intent to detailed post activity

--- app/src/main/java/com/example/dipansh/the_boring_blog_android/PostAdapter.java
+++ app/src/main/java/com/example/dipansh/the_boring_blog_android/PostAdapter.java
@@ -2,6 +2,7 @@ package com.example.dipansh.the_boring_blog_android;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.text.TextUtils;
 import android.view.View;
@@ -10,6 +11,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.Serializable;
 import java.util.List;
 
 import static android.text.TextUtils.substring;
@@ -29,11 +31,22 @@ public class PostAdapter extends ArrayAdapter<Post> {
             convertView = ((Activity) getContext()).getLayoutInflater().inflate(R.layout.post_item, parent, false);
         }
 
-        Post post = getItem(position);
+        final Post post = getItem(position);
         TextView title = convertView.findViewById(R.id.title);
         TextView content = convertView.findViewById(R.id.content);
         TextView date = convertView.findViewById(R.id.date);
         TextView category = convertView.findViewById(R.id.category);
+        TextView readmore = convertView.findViewById(R.id.readmore);
+
+        readmore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Post sendPost = post;
+                Intent i = new Intent(v.getContext(), ScrollingActivity.class);
+                i.putExtra("myPost", sendPost);
+                v.getContext().startActivity(i);
+            }
+        });
 
         String data = post.getContent();
         data = TextUtils.substring(data, 0, Math.min(data.length(), 191));
